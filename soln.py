3d55cde8b7f9827466c14260e5315be84f75572f
Jared Jonckheere <jonckh10@msu.edu>
2012-04-18 16:27:22 -0400

    systemdir.py operational. added dummy soln.py
    Added test hps files.

---
+++ soln.py
@@ -0,0 +1,3 @@
+class Soln:
+    def __init__(self, filename):
+        self.filename = filename
--- systemdir.py
+++ systemdir.py
@@ -1,28 +1,61 @@
-class Path:
-    soln = None
-    logs = {} 
+import os
+from log import Log
+from soln import Soln
+
+class Entry:
+    def __init__(self):
+        self.soln = None
+        self.logs = {} 
+
+    def __str__(self):
+        retStr = self.soln.filename+ "\n\t"
+        for k in self.logs.keys():
+            retStr += k + " " + self.logs[k].filename + "\n\t"
+        return retStr
+
 
 class SystemDir:
-    SOLUTION_TYPES = ["soln", "failed","div"]
-
-    paths = {}
     def __init__(self, systemDir):
         self.systemDir = systemDir
+        self.entries = {}
 
         # read filenames and layout data 
         for fname in os.listdir(systemDir):
+            
+            if not self.validName(fname):
+                print "Invalid filename: ", fname
+                continue
+
             filePath = systemDir + '/' + fname
             fnameParts = fname.split('.')
-            pathNum = fParts[-1]
+            entryNum = fnameParts[-1]
             
             if fnameParts[0].startswith("log"): # log file
-                logType = fParts[1]
-                if self.paths[pathNum] == None: # path not initialized
-                    self.paths[pathNum] = Path()
-                self.paths[pathNum].logs[logtype] = Log(filepath)   
+                logType = fnameParts[1]
+                if not self.entries.has_key(entryNum): # entry not initialized
+                    self.entries[entryNum] = Entry()
+                self.entries[entryNum].logs[logType] = Log(filePath)   
 
             else:
-                if self.paths[pathNum] == None:
-                    self.paths[pathNum].soln = Path()
-                self.paths[pathNum].soln = Soln(filepath)
+                if not self.entries.has_key(entryNum):
+                    self.entries[entryNum] = Entry()
+                self.entries[entryNum].soln = Soln(filePath)
+
+
+    def validName(self, fname):
+        valid = True
+        fnameParts = fname.split('.')
+        #correct number of parts in filename
+        if fnameParts[0] == 'log':
+            valid = valid and len(fnameParts) == 3
+        else:
+            valid = valid and len(fnameParts) == 2
+        #last part of filename is digit
+        valid = valid and fnameParts[-1].isdigit()
+        return valid
+
+
+s = SystemDir("hps")
+for k in s.entries.keys():
+    print k, s.entries[k]
 
