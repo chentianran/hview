4a8bd5878bf7c763704ad6731dd9b1d137f3aa6d
Jared Jonckheere <jonckh10@msu.edu>
2012-04-19 20:38:33 -0400

    Added entries_by_type and entries_by_value methods

--- systemdir.py
+++ systemdir.py
@@ -29,15 +29,14 @@ class SystemDir:
             entryNum = fnameParts[-1]
 
             if not self.entries.has_key (entryNum):                 # record info 
-                self.entries[entryNum] = [Entry(filePath)]
+                self.entries[entryNum] = {entryType:Entry(filePath)}
             else:
-                self.entries[entryNum].append (Entry(filePath))
+                self.entries[entryNum][entryType] = Entry(filePath)
 
             if not self.types.has_key (entryType):
                 self.types[entryType] = set ([entryNum])
             else:
                 self.types[entryType].add (entryNum)
-            
 
     def validName(self, fname):
         valid = True
@@ -51,8 +50,33 @@ class SystemDir:
         valid = valid and fnameParts[-1].isdigit()
         return valid
 
+    #return a list of all the entries associated wiht an id
+    def entries_by_id(self,entryId):
+        return self.entries[entryId].values()
+
+
+    #should only re
+    def entries_by_type(self,entryType):
+        return entriesByTypeIter(entryType, self)
+
+
+class entriesByTypeIter(object):
+    def __init__(self, entryType, systemsDir):
+        self.systemsDir = systemsDir
+        self.entryType = entryType
+        self.current_item = 0
+        self.ids = sorted(list(systemsDir.types[self.entryType]))
+    def __iter__(self):  
+        return self
+    def next(self):
+        if (self.current_item == len(self.ids)):
+            raise StopIteration
+        else:
+            entryId = self.ids[self.current_item]
+            entry = self.systemsDir.entries[entryId][self.entryType]
+            self.current_item += 1
+            return entry
 
 s = SystemDir("hps")
-for k in s.types.keys():
-    print k, s.types[k]
-
+for k in s.entries_by_id("0"):
+    print k
